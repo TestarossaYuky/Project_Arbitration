45bdc5e8fe06a16aec82cc4b33e709c94ea8b278
Savary Gabriel <tpegaby@gmail.com>
2017-05-03 20:17:26 +0200

    add change scene handball
    quand on appuis sur le bouton hanball , on change arrive sur la scene handball

---
+++ Scoring/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void on_Handball_click()
+	{
+
+		SceneManager.LoadScene ("Handball");
+	}
+}
