5f44d2e0a8bddeed77f945dcb0a7f74231d756e5
Hugo <h.lagarde@student.isartdigital.com>
2017-06-28 20:30:27 +0200

    Recup defrag Computer
    J'ai pu recuperer ma plus vieille save lors de la defrag du pc :( désoler la je  pars pour 4-5h de reconstruction si je finis vite j essaye de vous clear le truc

--- Scoring/Assets/Scripts/MainMenu.cs
+++ Scoring/Assets/Scripts/MainMenu.cs
@@ -6,6 +6,11 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour {
 
+    public Text pressText;
+
+    private float waitHide = 0.5f;
+    private float waitShow = 0.5f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,12 +18,30 @@ public class MainMenu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (pressText.IsActive() == true)
+        {
+            waitHide -= 0.03f;
+
+            if (waitHide <= 0.0f)
+            {
+                pressText.enabled = false;
+                waitHide = 0.5f;
+            }
+        }
+        else
+        {
+            waitShow -= 0.03f;
+            if ( waitShow <= 0.0f)
+            {
+                pressText.enabled = true;
+                waitShow = 0.5f;
+            }
+        }
 		
 	}
 
-	public void on_Handball_click()
-	{
-
-		SceneManager.LoadScene ("Handball");
-	}
+    public void on_Start_press()
+    {
+        SceneManager.LoadScene("SelectSport");
+    }
 }
